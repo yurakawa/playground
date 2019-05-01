121d7ac6b70f1dbbe9be8b3d0b8e1d11d5363c9f
yurakawa <s08a2015@gmail.com>
2019-05-02 01:38:35 +0900

    add external interruption

--- generator_pattern/main.go
+++ generator_pattern/main.go
@@ -1,20 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+)
 
 func main() {
-	for i := range Count(1, 99) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	for i := range Count(ctx, 1, 99) {
 		fmt.Println(i)
 	}
 }
 
-func Count (start, end int) <- chan int{
+func Count (ctx context.Context, start, end int) <- chan int{
 	ch := make(chan int)
-	go func(ch chan int) {
+	go func(ch chan<-int) {
+		defer close(ch)
+	loop:
 		for i := start; i <= end; i++ {
+
+			select {
+			case <-ctx.Done():
+				break loop
+			default:
+			}
+
+			// 重たい処理
+			time.Sleep(500 * time.Millisecond)
 			ch <- i
 		}
 	}(ch)
-
 	return ch
 }
