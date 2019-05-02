88949bffda59b638738c90530a69d45bf9d88744
yurakawa <s08a2015@gmail.com>
2019-05-02 17:52:42 +0900

    ランダム選択のCLIツール

---
+++ choice/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main () {
+	// 乱数の種を初期化
+	rand.Seed(time.Now().UnixNano())
+
+	// コマンドライン引数の数をカウント
+	c := len(os.Args) - 1
+
+	if c < 1 {
+		fmt.Fprintf(os.Stderr, "[usage] %s choice1 choice2...", os.Args[0])
+		os.Exit(1)
+	}
+
+	// 乱数で選択したものを表示
+	fmt.Printf(os.Args[rand.Intn(c) + 1])
+
+	hoge := os.Args
+	fmt.Println(hoge)
+}
+
+
