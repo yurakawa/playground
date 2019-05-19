f9e0492237c2c5f75462213b938709232e0352ff
yurakawa <s08a2015@gmail.com>
2019-05-19 09:51:42 +0900

    シャッフル

---
+++ shuffle/example/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/yurakawa/playground/shuffle"
+)
+
+type Dice []int
+
+func (d Dice) Seed() int64   { return int64(os.Getpid()) }
+func (d Dice) Len() int      { return len(d) }
+func (d Dice) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
+
+var dice = Dice([]int{1, 2, 3, 4, 5, 6})
+
+func main() {
+	fmt.Printf("%v\n", dice)
+	shuffle.Shuffle(dice)
+	fmt.Printf("%v\n", dice)
+
+}
