73cde444e48e2aef10674e5ef17e3e4f40ea9c63
yurakawa <s08a2015@gmail.com>
2019-07-27 12:17:23 +0900

    add error handling

---
+++ concurrency-in-go/concurrency-patterns-in-go/error-handling/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	good()
+}
+
+func good() {
+	type Result struct {
+		Error error
+		Response *http.Response
+	}
+
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				result := Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for result := range checkStatus(done, urls...) {
+		if result.Error != nil {
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response.Status)
+	}
+}
+
+// エラーが有ることは表示できるがエラーを戻せない
+func bad() {
+	checkStatus := func(done <-chan interface{}, urls ...string) <-chan *http.Response {
+		responses := make(chan *http.Response)
+		go func() {
+			defer close(responses)
+			for _, url := range urls {
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				select {
+				case <-done:
+					return
+				case responses <- resp:
+				}
+			}
+		}()
+		return responses
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	urls := []string{"https://www.google.com", "https://badhost"}
+	for response := range checkStatus(done,urls...) {
+		fmt.Printf("Response: %v\n", response.Status)
+	}
+}
